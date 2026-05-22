48a8c2f686fbb126a18e7fd5b2a6e7586565f6f7
José Paulino <josepaulinog@gmail.com>
2026-05-21 21:01:53 -0400

    Gallery hero: swap to FISC 2025 Dili Day 3 photo
    Replaces the Day 1 still with the Day 3 Tuesday capture from last
    year's Dili edition. Old asset deleted to keep src/imports tidy.

--- src/app/data.ts
+++ src/app/data.ts
@@ -25,7 +25,7 @@ import maracasCabin from "../imports/maracas-cabin.jpg";
 import scarletIbis from "../imports/scarlet-ibis.jpg";
 import queensRoyalCollege from "../imports/queens-royal-college.jpg";
 import carnivalDancer from "../imports/carnival-dancer.jpg";
-import fisc2025DiliDay1 from "../imports/fisc-2025-dili-day1.jpg";
+import fisc2025DiliDay3 from "../imports/fisc-2025-dili-day3.jpg";
 import speakersImage from "../imports/speakers5.png";
 import newsletterImage from "../imports/newsletter.jpg";
 
@@ -74,7 +74,7 @@ export const HERO_RESOURCES = agendaImage;
 export const HERO_ATTENDEES = aboutImage;
 export const HERO_MATERIALS = agendaImage;
 export const HERO_GUIDE = maracasBeach;
-export const HERO_GALLERY = fisc2025DiliDay1;
+export const HERO_GALLERY = fisc2025DiliDay3;
 export const HERO_VIDEOS = carnivalDancer;
 export const HERO_MEDIA = portOfSpainAerial;
 // Sign-in background is a purpose-generated Replicate image (no Trinidad
